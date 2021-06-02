b9d246192328d413a4390c88d97f0c488458427c
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2021-06-02 10:13:15 +0100

    - renamed service

---
+++ gRPC_REST/Services/DataService.cs
@@ -0,0 +1,24 @@
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace gRPC_REST
+{
+  public class DataServiceImpl : DataService.DataServiceBase
+  {
+    private readonly ILogger<DataServiceImpl> _logger;
+
+    public DataServiceImpl(ILogger<DataServiceImpl> logger)
+    {
+      _logger = logger;
+    }
+
+    public override Task<DataResponse> GetData(DataRequest request, ServerCallContext context)
+    {
+      return Task.FromResult(new DataResponse
+      {
+        Message = "Hello " + request.Name
+      });
+    }
+  }
+}
--- gRPC_REST_Client/Program.cs
+++ gRPC_REST_Client/Program.cs
@@ -11,9 +11,9 @@ namespace gRPC_REST_Client
     {
       // The port number(5001) must match the port of the gRPC server.
       using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-      var client = new Greeter.GreeterClient(channel);
-      var request = new HelloRequest { Name = "GreeterClient" };
-      var reply = await client.SayHelloAsync(request);
+      var client = new DataService.DataServiceClient(channel);
+      var request = new DataRequest { Name = "GreeterClient" };
+      var reply = await client.GetDataAsync(request);
 
       Console.WriteLine("Greeting: " + reply.Message);
       Console.WriteLine("Press any key to exit...");
