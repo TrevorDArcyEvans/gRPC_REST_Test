1ca1d27daeded5e42af7ff2e973c3720138f8c29
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2021-06-02 11:58:32 +0100

    - added REST API

---
+++ gRPC_REST/Controllers/DataServiceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gRPC_REST.Controllers
+{
+  [Route("api/[controller]")]
+  [Produces("application/json")]
+  public class DataServiceController : ControllerBase
+  {
+    [HttpGet]
+    [Route(nameof(GetData) + "/{chunk:int}")]
+    public ActionResult<DataResponse> GetData(
+      [FromRoute] [Required] int chunk)
+    {
+      var response = new DataResponse
+      {
+        Message = "Requested:  " + chunk
+      };
+      Enumerable.Range(0, chunk).ToList().ForEach(i =>
+      {
+        var dp = new DataPacket
+        {
+          Name = "Name: " + Guid.NewGuid(),
+          Id = Guid.NewGuid().ToString(),
+          SourceId = Guid.NewGuid().ToString(),
+          TargetId = Guid.NewGuid().ToString(),
+          OrganisationId = Guid.NewGuid().ToString(),
+          RepositoryId = Guid.NewGuid().ToString(),
+          OwnerId = Guid.NewGuid().ToString(),
+          NextId = Guid.NewGuid().ToString(),
+        };
+        response.Payload.Add(dp);
+      });
+
+      return Ok(response);
+    }
+  }
+}
--- gRPC_REST/Startup.cs
+++ gRPC_REST/Startup.cs
@@ -1,7 +1,7 @@
 ﻿using gRPC_REST.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,9 +13,15 @@ namespace gRPC_REST
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddControllers();
+      services.AddResponseCompression(options =>
+      {
+        options.EnableForHttps = true;
+        options.Providers.Add<GzipCompressionProvider>();
+      });
       services.AddGrpc(options =>
       {
-        options.MaxSendMessageSize = null;  // null = remove limit!
+        options.MaxSendMessageSize = null; // null = remove limit!
         options.MaxReceiveMessageSize = 100 * 1024 * 1024; // 100 MB
       });
     }
@@ -29,15 +35,12 @@ namespace gRPC_REST
       }
 
       app.UseRouting();
+      app.UseResponseCompression();
 
       app.UseEndpoints(endpoints =>
       {
+        endpoints.MapControllers();
         endpoints.MapGrpcService<DataServiceImpl>();
-
-        endpoints.MapGet("/", async context =>
-        {
-          await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-        });
       });
     }
   }
